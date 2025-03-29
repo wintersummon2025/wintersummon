cef14da4080b15cf60b3fb0543204bccef4082ab
wintersummon2025 <ganagallachetan222@gmail.com>
2025-03-29 17:08:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -82,9 +82,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "WinterSummon Bot is running happily!"
-
+    
 def run_flask():
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
 
 def keep_alive():
     t = threading.Thread(target=run_flask)
